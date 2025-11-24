adriangdp/thejobseekersfate

commit 88af7848946260f97d239825f9228b72c85011c9

- Added <Modal> as wrapper for modal dialogues - Updated <Board> and <AddApplication> to make use of this. - Added parent compontent <Board> scroll lock if a modal is open.

src/lib/AddApplication.svelte
@@ -7,8 +7,6 @@
     import type { JobState } from "@data/types";
     import { keyof } from "zod";
 
-
-
     let{ showAddApplicationModal = $bindable() } = $props()
   
     let selectedStatus:string = $state("offer")
@@ -35,8 +33,7 @@
     }
 
 </script>
-<div class="z-40 absolute top-0 left-0 w-screen h-screen p-40 bg-accent-inner/20 backdrop-blur-xs">
-    <div class="relative bg-accent-inner/90 border-2 rounded-md border-accent-outer">
+
         <form onsubmit={handleNewApplication}>
             <div class="flex items-center gap-1 m-5">
                 <div>
@@ -72,8 +69,5 @@
             </div> 
             <button type="submit" class="w-full h-full"> Apply here</button>
         </form>
-        <CloseButton bind:isOpen={showAddApplicationModal} positioning="absolute top-0 right-0"/>         
-    </div>  
-          
-</div>
+
 

src/lib/Board.svelte
@@ -6,6 +6,7 @@
     import AddApplication from "./AddApplication.svelte"
     import BoardFilter from "@lib/BoardFilter.svelte"
     import JobDisplay from "@lib/JobDisplay.svelte"
+    import Modal from "@lib/Modal.svelte"
     
     let isCardDisplay: boolean = $state(true)
 
@@ -16,6 +17,8 @@
     let showOffer: boolean = $state(true)
     let showAccepted: boolean = $state(true)
     let showGhosted: boolean = $state(true)
+
+    let preventScroll: boolean = $derived(showAddApplicationModal)
 
     let sortBy:string = $state("default")
     let isAscendent:boolean = $state(false)
@@ -66,52 +69,58 @@
     )
 </script>
 
-<div class="flex gap-2">
-<button onclick={()=>{isCardDisplay = !isCardDisplay}}> Change view</button>
-<button onclick={()=>{showAddApplicationModal = !showAddApplicationModal}}> Add Job Application</button>
-</div>
-<BoardFilter 
-    bind:showRejected 
-    bind:showApplied 
-    bind:showInterview 
-    bind:showAccepted
-    bind:showOffer
-    bind:showGhosted
-    bind:sortBy 
-    bind:isAscendent
->
-</BoardFilter>
-{#if filteredApplications}
-    {#if isCardDisplay}
-        <div class="grid grid-cols-2 md:grid-cols-2 gap-3">
-            {#each filteredApplications as cardApplication}
-                <JobDisplay application={cardApplication} isCardDisplay={true}></JobDisplay>
-            {/each}
-        </div>
-    {:else}
-        <table>
-            <thead>
-                <tr>
-                    <th>Status</th>
-                    <th>Position</th>
-                    <th>Company</th>
-                    <th>Mode</th>
-                    <th>Salary</th>
-                    <th>Link</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                {#each filteredApplications as application }
-                    <JobDisplay application={application} isCardDisplay={false}></JobDisplay>
+<div class={`${preventScroll ? 'h-screen overflow-hidden':''}`}>
+    <div class="flex gap-2">
+        <button onclick={()=>{isCardDisplay = !isCardDisplay}}> Change view</button>
+        <button onclick={()=>{showAddApplicationModal = !showAddApplicationModal}}> Add Job Application</button>
+    </div>
+    <BoardFilter 
+        bind:showRejected 
+        bind:showApplied 
+        bind:showInterview 
+        bind:showAccepted
+        bind:showOffer
+        bind:showGhosted
+        bind:sortBy 
+        bind:isAscendent
+    >
+    </BoardFilter>
+    {#if filteredApplications}
+        {#if isCardDisplay}
+            <div class="grid grid-cols-2 md:grid-cols-2 gap-3">
+                {#each filteredApplications as cardApplication}
+                    <JobDisplay application={cardApplication} isCardDisplay={true}></JobDisplay>
                 {/each}
-        
-            </tbody>
-        </table>
+            </div>
+        {:else}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Status</th>
+                        <th>Position</th>
+                        <th>Company</th>
+                        <th>Mode</th>
+                        <th>Salary</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                    {#each filteredApplications as application }
+                        <JobDisplay application={application} isCardDisplay={false}></JobDisplay>
+                    {/each}
+            
+                </tbody>
+            </table>
+        {/if}
     {/if}
-{/if}
-{#if showAddApplicationModal}
-    <AddApplication bind:showAddApplicationModal />
-{/if}
+    {#if showAddApplicationModal}
+    <Modal bind:isOpen={showAddApplicationModal}>
+        <AddApplication />
+    </Modal>
 
-<button onclick={clearTable}> Clear data</button>
+    {/if}
+
+    <button onclick={clearTable}> Clear data</button>
+</div>
+

src/lib/Modal.svelte
@@ -1,0 +1,13 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import CloseButton from "@lib/common/CloseButton.svelte";
+    let {isOpen = $bindable(), children} : {isOpen:boolean, children:Snippet}= $props()
+</script>
+
+<div class="z-40 fixed top-0 left-0 w-screen h-screen p-40 bg-accent-inner/20 backdrop-blur-xs">
+    <div class="relative bg-accent-inner/90 border-2 rounded-md border-accent-outer">
+        {@render children()}
+        <CloseButton bind:isOpen positioning="absolute top-0 right-0"/>         
+    </div>  
+          
+</div>

commit 61633499d69407e20976065905c858097601c366

Further preparing grounds for styling and 1 bugfix
- Added key figureName to type JobState to display the simbolical value of the situation value
- Added and removed some icons in /public
- Updated JobStates dictionary and informed new figureName key/value.
- Updated JobStates dictionary icon paths
- Fixed AddApplication modal not closing after submitting new job application (small bug introduced in 88af7848946260f97d239825f9228b72c85011c9)
- Some housekeeping and mop up of leftover imports and debugging messages.

src/data/jobStates.ts
@@ -3,7 +3,8 @@ import type { JobState, ReadOnlyDictionary} from "@data/types";
 export const JobStates:ReadOnlyDictionary<JobState> = {
     offer:{
         situation:"Offer",
-        icon: "/img/icon-star.png",
+        figureName:"The Wheel of Fortune",
+        icon: "/img/icon-fortunewheel.png",
         styles:{
             color:"color-card-offer",
             background:"bg-card-offer",
@@ -19,7 +20,8 @@ export const JobStates:ReadOnlyDictionary<JobState> = {
 
     applied:{
         situation:"Applied",
-        icon: "/img/icon-tower.png",
+        figureName:"The Waiting",
+        icon: "/img/icon-sandclock.png",
         styles:{
             color:"color-card-applied",
             background:"bg-card-applied",
@@ -34,6 +36,7 @@ export const JobStates:ReadOnlyDictionary<JobState> = {
     },
     rejected:{
         situation:"Rejected",
+        figureName:"The Reaper",
         icon: "/img/icon-reaper.png",
         styles:{
             color:"color-card-rejected",
@@ -49,6 +52,7 @@ export const JobStates:ReadOnlyDictionary<JobState> = {
     },
     interview:{
         situation:"Interview",
+        figureName:"The Judgement",
         icon: "/img/icon-scale.png",
         styles:{
             color:"color-card-interview",
@@ -64,7 +68,8 @@ export const JobStates:ReadOnlyDictionary<JobState> = {
     },
     ghosted:{        
         situation:"Ghosted",
-        icon: "/img/icon-scale.png",
+        figureName:"The Ghost",
+        icon: "/img/icon-ghost.png",
         styles:{
             color:"color-card-ghosted",
             background:"bg-card-ghosted",
@@ -79,7 +84,8 @@ export const JobStates:ReadOnlyDictionary<JobState> = {
     },
     accepted:{
         situation:"Accepted",
-        icon: "/img/icon-scale.png",
+        figureName:"The Star",
+        icon: "/img/icon-star.png",
         styles:{
             color:"color-card-accepted",
             background:"bg-card-accepted",

src/data/types.ts
@@ -25,6 +25,7 @@ export type JobData = {
 
 export type JobState ={
     situation:string,
+    figureName:string,
     icon:string,
     styles:{
         color:string,

src/lib/AddApplication.svelte
@@ -2,12 +2,9 @@
     import { db } from "@data/db"
     import { JobStates } from "@data/jobStates"
     import { applicationSchema } from "@data/validation-schemas"
-    import CloseButton from "@lib/common/CloseButton.svelte"
     import BadgeStatus from "@lib/common/BadgeStatus.svelte";
-    import type { JobState } from "@data/types";
-    import { keyof } from "zod";
 
-    let{ showAddApplicationModal = $bindable() } = $props()
+    let{ showAddApplicationModal = $bindable() } : {showAddApplicationModal : boolean}= $props()
   
     let selectedStatus:string = $state("offer")
 
@@ -24,7 +21,6 @@
             const id = await db.jobData.add(parseData)
             
             form.reset()
-            selectedStatus = "offer"
             showAddApplicationModal = false
         } catch(error){
             console.log(error)

src/lib/Board.svelte
@@ -116,7 +116,7 @@
     {/if}
     {#if showAddApplicationModal}
     <Modal bind:isOpen={showAddApplicationModal}>
-        <AddApplication />
+        <AddApplication bind:showAddApplicationModal={showAddApplicationModal} />
     </Modal>
 
     {/if}

src/lib/JobDetail.svelte
@@ -1,0 +1,14 @@
+<script lang="ts">
+    import { type JobApplication, type JobState } from "@data/types";
+    let  {jobApplication } : {jobApplication: JobApplication} = $props()
+</script>
+
+<div class="flex flex-col">
+    <div class="flex">
+        <img src={jobApplication.status.icon} alt={jobApplication.status.situation} />
+        <div>
+            <h3>{jobApplication.company}</h3>
+            <p>{jobApplication.company}</p>
+        </div>
+    </div>
+</div>

src/lib/JobDisplay.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-    import { type JobState, type JobApplication } from "../data/types"
-    import { JobStates } from "@data/jobStates"
+    import { type JobApplication } from "../data/types"
     import BadgeStatus from "@lib/common/BadgeStatus.svelte"
-    import iconReaper from "/img/icon-reaper.png"
-    import iconScale from "/img/icon-scale.png"
-    import iconStar from "/img/icon-star.png"
-    import iconTower from "/img/icon-tower.png"
 
 
    let { application, isCardDisplay } : {application:JobApplication, isCardDisplay:boolean}  = $props();
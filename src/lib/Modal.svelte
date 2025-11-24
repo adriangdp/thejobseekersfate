8fedc3d352aae1fc3f69bd52a37dac3f17775d26
Adrián Gallardo <adriangdp@gmail.com>
2025-11-24 12:18:10 +0100

    - Added <Modal> as wrapper for modal dialogues - Updated <Board> and <AddApplication> to make use of this.

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import CloseButton from "@lib/common/CloseButton.svelte";
+    let {isOpen = $bindable(), children} : {isOpen:boolean, children:Snippet}= $props()
+</script>
+
+<div class="z-40 fixed top-0 left-0 w-screen h-screen p-40 bg-accent-inner/20 backdrop-blur-xs">
+    <div class="relative bg-accent-inner/90 border-2 rounded-md border-accent-outer">
+        {@render children()}
+        <CloseButton bind:isOpen positioning="absolute top-0 right-0"/>         
+    </div>  
+          
+</div>

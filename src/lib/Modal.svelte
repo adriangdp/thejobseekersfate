52b4eaa78d28acdcca6e76eaa2323961a9e2e21f
Adrián Gallardo <adriangdp@gmail.com>
2025-12-04 19:54:09 +0100

    Quick change on <Modal> from parent <div> to the more fitting <dialog>

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -4,10 +4,10 @@
     let {isOpen = $bindable(), children} : {isOpen:boolean, children:Snippet}= $props()
 </script>
 
-<div class="z-40 flex items-center justify-center fixed top-0 left-0 w-screen h-full bg-accent-inner/60 backdrop-blur-xs">
+<dialog class="z-40 flex items-center justify-center fixed top-0 left-0 w-screen h-full bg-accent-inner/60 backdrop-blur-xs">
     <div class="relative bg-accent-inner/90 border-2 rounded-md border-accent-outer max-w-full max-h-full px-12 md:px-15 py-14 md:py-18 overflow-x-hidden overflow-y-scroll lg:overflow-y-visible">
         {@render children()}
         <CloseButton bind:isOpen positioning=""/>         
     </div>  
           
-</div>
+</dialog>

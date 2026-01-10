5bdb735135425867d19af9784bae52e461f6d66e
Adrián Gallardo <adriangdp@gmail.com>
2026-01-10 18:07:41 +0100

    feat(testing): Add testing with vitest, testing library and jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode })=>({
   plugins: [svelte(), tailwindcss()],
   resolve:{
     alias:{
       "@assets":path.resolve(__dirname,"./src/components" ),
       "@lib":path.resolve(__dirname, "./src/lib"),
       "@data":path.resolve(__dirname, "./src/data")
-    }
-  }
-})
+    },
+
+  },
+  test: {
+    environment: 'jsdom',
+  },
+  }))

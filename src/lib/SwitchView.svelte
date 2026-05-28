a0ced52e0ac629d4f7944f65b98a1cb6dfb5a49a
Adrián Gallardo <adriangdp@gmail.com>
2026-05-28 17:56:37 +0200

    style(css): adjust view icon sizes

--- src/lib/SwitchView.svelte
+++ src/lib/SwitchView.svelte
@@ -6,13 +6,13 @@
 
 <div class="flex gap-0 bg-accent-inner rounded-md p-1">
     <button onclick={()=>{isCardDisplay = true}}
-        class={`flex w-full h-full button-invisible ${isCardDisplay? 'bg-accent-outer':''} rounded-md hover:bg-accent-outer/50 p-1 group`}  
+        class={`button-invisible ${isCardDisplay? 'bg-accent-outer':''} rounded-md hover:bg-accent-outer/50 p-1 group`}  
     >
-        <img width="24px" height="24px" src="/img/icon-view-card.png" alt="Card view style icon" class={`group-hover:brightness-200 ${isCardDisplay? 'brightness-200' : 'brightness-100'}`}/>
+        <img src="/img/icon-view-card.png" alt="Card view style icon" class={`w-8 h-8 group-hover:brightness-200 ${isCardDisplay? 'brightness-200' : 'brightness-100'}`}/>
     </button>
     <button  onclick={()=>{isCardDisplay = false}}
-        class={`flex w-full h-full button-invisible ${!isCardDisplay? 'bg-accent-outer':''} rounded-md hover:bg-accent-outer/50 p-1 group`}
+        class={`button-invisible ${!isCardDisplay? 'bg-accent-outer':''} rounded-md hover:bg-accent-outer/50 p-1 group`}
     >
-        <img width="24px" height="24px" src="/img/icon-view-table.png" alt="Table view style icon" class={`group-hover:brightness-200 ${!isCardDisplay? 'brightness-200' : 'brightness-100'}`}/>
+        <img src="/img/icon-view-table.png" alt="Table view style icon" class={`w-8 h-8 group-hover:brightness-200 ${!isCardDisplay? 'brightness-200' : 'brightness-100'}`}/>
     </button>
 </div>

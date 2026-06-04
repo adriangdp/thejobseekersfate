7b34e286e6fce1150787a6973eca61f86f1ef96d
Adrián Gallardo <adriangdp@gmail.com>
2026-06-04 16:22:53 +0200

    refactor: recieve background color as optional prop. Remove background from CloseButton

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
     import type { Snippet } from "svelte";
     import CloseButton from "@lib/common/CloseButton.svelte";
-    let {isOpen = $bindable(), children} : {isOpen:boolean, children:Snippet}= $props()
+
+    let {isOpen = $bindable(), backgroundColor, children} : {isOpen:boolean, backgroundColor?:string, children:Snippet}= $props()
     let isClosing : boolean = $state(false);
+    let backgroundAlpha = $derived(backgroundColor ? backgroundColor:"bg-accent-outer/90")
+
     const handleClose = () =>{
         isClosing = true;
         setTimeout(()=>{isOpen = false}, 400);
     }
+
 </script>
 
-<dialog class="z-30 flex items-center justify-center fixed top-0 left-0 w-screen h-full bg-accent-inner/60 backdrop-blur-xs">
-    <div class={`relative bg-accent-inner/90 border-2 rounded-md border-accent-outer max-w-full max-h-full px-12 md:px-15 py-14 md:py-18 overflow-x-hidden overflow-y-scroll lg:overflow-y-visible ${isClosing ? 'animate-falldown-reverse':'animate-falldown'}`}>
+<dialog class="z-30 flex items-center justify-center fixed top-0 left-0 w-screen h-full bg-accent-outer/60 backdrop-blur-xs">
+    <div class={`relative ${backgroundAlpha} rounded-md max-w-full max-h-full px-12 md:px-15 py-14 md:py-18 overflow-x-hidden overflow-y-scroll lg:overflow-y-visible ${isClosing ? 'animate-falldown-reverse':'animate-falldown'}`}>
         {@render children()}
         <CloseButton handler={handleClose} positioning=""/>         
     </div>            
--- src/lib/common/CloseButton.svelte
+++ src/lib/common/CloseButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button onclick={handler}
-    class={`${positioning  || 'absolute top-2 right-2'} border-0 text-xl font-bold cursor-pointer` }     
+    class={`${positioning  || 'absolute top-2 right-2'} border-0 text-xl font-bold cursor-pointer bg-transparent` }     
 ><img src="/img/icon-delete.png" alt="close button icon"
     class="w-8 md:w-12"
 /></button>

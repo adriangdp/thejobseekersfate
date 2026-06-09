33713888603fb9d39b09c7c58ed9dfc9ff6d68af
Adrián Gallardo <adriangdp@gmail.com>
2026-06-09 21:35:55 +0200

    refactor: change hex to oklh. Add color lighter and darker variants. Update related types. Update font

--- src/data/jobStates.ts
+++ src/data/jobStates.ts
@@ -13,8 +13,9 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-offer",
             bgGradient:"",
             badge:{
-                border:"text-card-offer/20",
-                background:"bg-card-offer/20",
+                text:"text-card-offer-lighter",
+                border:"border-card-offer-lighter",
+                background:"bg-card-offer-darker",
             }
         }
     },
@@ -30,8 +31,9 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-applied",
             bgGradient:"bg-radial-[at_50%_35%] from-card-applied/55 to-card-applied/75 to-40% brightness-65",
             badge:{
-                border:"text-card-applied/20",
-                background:"bg-card-applied/20",
+                text:"text-card-applied-lighter",
+                border:"border-card-applied-lighter",
+                background:"bg-card-applied-darker",
             }
         }
     },
@@ -46,8 +48,9 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-rejected",
             bgGradient:"bg-radial-[at_50%_35%] from-card-rejected/45 to-card-rejected/65 to-40% brightness-65",
             badge:{
-                border:"text-card-rejected/20",
-                background:"bg-card-rejected/20",
+                text:"text-card-rejected-lighter",
+                border:"border-card-rejected-lighter",
+                background:"bg-card-rejected-darker",
             }
         },
     },
@@ -62,15 +65,16 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-interview",
             bgGradient:"bg-radial-[at_50%_35%] from-card-interview/60 to-card-interview/80 to-40% brightness-65",
             badge:{
-                border:"text-card-interview/20",
-                background:"bg-card-interview/20",
+                text:"text-card-interview-lighter",
+                border:"border-card-interview-lighter",
+                background:"bg-card-interview-darker",
             }
         }
     },
     ghosted:{        
         state: enumJobStatus.ghosted,
         figureName:"The Ghost",
-        icon: "/img/icon-ghost.png",
+        icon: "/img/icon-ghost.svg",
         styles:{
             color:"color-card-ghosted",
             background:"bg-card-ghosted",
@@ -78,8 +82,9 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-ghosted",
             bgGradient:"bg-radial-[at_50%_35%] from-card-ghosted/60 to-card-ghosted/80 to-40% brightness-65",
         badge:{
-                border:"text-card-ghosted/20",
-                background:"bg-card-ghosted/20",
+                text:"text-card-ghosted-lighter",
+                border:"border-card-ghosted-lighter",
+                background:"bg-card-ghosted-darker",
             }
         }
     },
@@ -94,8 +99,9 @@ export const JobStyling:JobStylingDictionary = {
             text:"text-card-accepted",
             bgGradient:"bg-radial-[at_50%_35%] from-card-accepted/60 to-card-accepted/80 to-40% brightness-65",
         badge:{
-                border:"text-card-accepted/20",
-                background:"bg-card-accepted/20",
+                text:"text-card-accepted-lighter",
+                border:"border-card-accepted-lighter",
+                background:"bg-card-accepted-darker",
             }
         }
     }
--- src/data/types.ts
+++ src/data/types.ts
@@ -19,6 +19,7 @@ export type JobStyle ={
         text:string,
         bgGradient:string
         badge:{
+            text:string,
             border:string,
             background:string,
         }

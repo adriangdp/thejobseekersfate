2de5ff1ada0bbc43a2117d2cfb286475a32caaaa
Adrián Gallardo <adriangdp@gmail.com>
2026-06-15 15:32:33 +0200

    fix: update test cases to new data types
    Test were failing ever since types were updated.

--- src/data/__test__/validation-schemas.svelte.test.ts
+++ src/data/__test__/validation-schemas.svelte.test.ts
@@ -1,57 +1,57 @@
-import { flushSync } from "svelte";
-import {describe, it, expect} from "vitest";
+import {describe, it, expect, expectTypeOf} from "vitest";
+import type { JobPost } from "@data/types";
+import { enumJobStatus } from "@data/enum";
 import { applicationSchema } from "@data/validation-schemas";
 import * as z from "zod";
 
 describe("applicationSchema",()=>{
     it("Returns a strongly typed deep clone of the input", ()=>{
         const date = new Date();
-        const mockData = {
-            statusKey: "Applied",
+        const mockData:JobPost = {
+            status: enumJobStatus.applied,
             position: "Front End Developer",
             company: "The Company ltd",
             mode:"remote",
             salary:30000,
             link:"http://google.com",
-            appliedDate: date
+            applied_date: date
         }
 
         const mock = applicationSchema.parse(mockData);
 
-        expect(mock).toMatchObject({
-            ...mockData,
-            salary:mockData.salary.toString()
-        })
+        expectTypeOf(mock).toEqualTypeOf<JobPost>()
+        expect(mock).toMatchObject(mockData)
     }),
     it("Returns a Zod Error", ()=>{
         const date = new Date();
         const mockErrorData = {
-            statusKey: "Applied",
-            position: null,
+            status: enumJobStatus.applied,
+            position: undefined,
             company: "The Company ltd",
             mode:"remote",
-            salary:"30000",
+            salary:30000,
             link:"http://google.com",
-            appliedDate: date.toISOString()
+            applied_date: date.toISOString()
         }
 
         expect(() => applicationSchema.parse(mockErrorData)).toThrow(z.ZodError);
     }),
     it("Returns a deep copy of mockData where salary is unknown", ()=>{
         const date = new Date();
-        const mockData = {
-            statusKey: "Applied",
+        const mockData:JobPost = {
+            status: enumJobStatus.applied,
             position: "Front End Developer",
             company: "The Company ltd",
             mode:"remote",
-            salary:null,
+            salary: undefined,
             link:"http://google.com",
-            appliedDate: date
+            applied_date: date
         }
 
         const mock = applicationSchema.parse(mockData);
 
-        expect(mock.salary).toBe("unknown")
+        expect(mock.salary).toBe(undefined)
     })
+    
     
 })
